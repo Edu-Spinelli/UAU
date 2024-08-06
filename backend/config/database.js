b1e4b1451a433d61023e7d15c27268ee1174a27c
Eduardo Henrique Spinelli <89799833+Edu-Spinelli@users.noreply.github.com>
2024-08-06 01:09:45 -0300

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@
 
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('Loja', 'root', '18637859', {
+const sequelize = new Sequelize('Loja', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
 });
